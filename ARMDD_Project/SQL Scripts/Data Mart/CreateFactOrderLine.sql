9f3861416786db3360707183f98b413220c9d169
Diogo Silva <1211882@isep.ipp.pt>
2025-12-30 21:52:13 +0000

    Changed PK on FactOrderLine

--- ARMDD_Project/SQL Scripts/Data Mart/CreateFactOrderLine.sql
+++ ARMDD_Project/SQL Scripts/Data Mart/CreateFactOrderLine.sql
@@ -1,14 +1,12 @@
 IF NOT EXISTS (SELECT 1 FROM sys.tables WHERE name = 'FactOrderLine')
 BEGIN
     CREATE TABLE dbo.FactOrderLine (
-        FactOrderLineKey INT IDENTITY(1,1) PRIMARY KEY,
-
-        ShipmentKey INT NOT NULL,
+        DateKey INT NOT NULL,
         CustomerKey INT NOT NULL,
         ProductKey INT NOT NULL,
         SiteKey INT NOT NULL,
         SizeKey INT NOT NULL,
-        DateKey INT NOT NULL,
+        ShipmentKey INT NOT NULL,
         CurrencyKey INT NOT NULL,
 
         OrderID INT NOT NULL,
@@ -19,8 +17,12 @@ BEGIN
         Discount MONEY,
         PromotionDiscount MONEY,
         ShippingCostAllocated MONEY,
-
         TotalLineValue_Local MONEY,
-        TotalLineValue_EUR MONEY
+        TotalLineValue_EUR MONEY,
+
+        CONSTRAINT PK_FactOrderLine PRIMARY KEY (
+            OrderID,
+            OrderLineID
+        )
     );
 END
